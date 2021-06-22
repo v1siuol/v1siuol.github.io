56bc256867f6babbba324b3216eb43c996296c62
v1siuol <v1siuol@users.noreply.github.com>
2021-06-22 00:47:58 +0000

    deploy: 0580e9d00ad99e0b0d2373ee44962f82b04ff1b7

---
+++ js/third-party/tags/pdf.js
@@ -0,0 +1,27 @@
+/* global NexT, CONFIG, PDFObject */
+
+document.addEventListener('page:loaded', () => {
+  if (document.querySelectorAll('.pdf-container').length) {
+    NexT.utils.getScript(CONFIG.pdf.object_url, {
+      condition: window.PDFObject
+    }).then(() => {
+      document.querySelectorAll('.pdf-container').forEach(element => {
+        PDFObject.embed(element.dataset.target, element, {
+          pdfOpenParams: {
+            navpanes : 0,
+            toolbar  : 0,
+            statusbar: 0,
+            pagemode : 'thumbs',
+            view     : 'FitH'
+          },
+          height      : element.dataset.height,
+          // forceIframe : true,  // Prevent Motion to hinder pdf rendering in chrome sometimes
+          fallbackLink: "<p>This browser does not support inline PDFs. Please download the PDF to view it: \
+                         <a href='[url]' rel='noopener' target='_blank'>Download PDF</a></p>",
+          // PDFJS_URL   : CONFIG.pdf.url,  // Disable pdf.js fallback; instead, we use fallbackLink
+          // forcePDFJS  : true  // buggy
+        });
+      });
+    });
+  }
+});
